46e0eec89713283dbdf1b2592a33499b6d2c1b10
Ginevra Fontes Williams <ginevra.f.williams@gmail.com>
2019-12-10 17:07:44 +0100

    still working on list edit

---
+++ test/controllers/lists_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class ListsControllerTest < ActionDispatch::IntegrationTest
+  test "should get edit" do
+    get lists_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get lists_update_url
+    assert_response :success
+  end
+
+end
