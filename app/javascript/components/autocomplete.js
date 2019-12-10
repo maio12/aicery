82b739ca75d5c8ddcedcbb12d3bc0366480c4382
Antonio Maiocchi <antonio.maiocchi12@gmail.com>
2019-12-10 18:44:30 +0100

    committing the search and show functionalities

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -3,13 +3,15 @@ import autocomplete from 'js-autocomplete';
 
 const autocompleteSearch = function() {
   const products = JSON.parse(document.getElementById('search-data').dataset.products)
-  const searchInput = document.getElementById('q');
+  const searchInput = document.getElementById('query');
 
   if (products && searchInput) {
+    console.log(products)
     new autocomplete({
       selector: searchInput,
       minChars: 1,
       source: function(term, suggest){
+        console.log("searching")
           term = term.toLowerCase();
           const choices = products;
           const matches = [];
