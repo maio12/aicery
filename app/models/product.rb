a33e22b0f0eebad18908756a246e03ee8e7048f3
patricio galindo <patriciogalindo@gmail.com>
2019-12-12 11:26:39 +0100

    added cloudinary

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
   @products = Product.pluck(:name).sort
+  mount_uploader :photo, PhotoUploader
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get 'pages/profile'
   devise_for :users
   root to: 'pages#home'
-  resources :lists, only: [:edit, :show, :index]
+  resources :lists, only: [:edit, :show, :index] do
+    get '/checkout', to: 'checkout#index'
+    get '/checkout/supermarket/:supermarket_id', to: 'supermarket#show'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
