5619df2baa4d5e432922ed1be97553df14aeba9f
gregory conti <gconti63@gmail.com>
2019-12-17 18:11:05 +0100

    trying to merge

--- app/models/supermarket.rb
+++ app/models/supermarket.rb
@@ -4,14 +4,12 @@ class Supermarket < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
-
   # basket is an array of arrays [:id, :quantity]
   def basket_total_price(basket)
     total = basket.inject(0) do |sum, item|
-      inventory = inventories.where(inventories: { product: item }).first
+      inventory = inventories.where(inventories: { product: item.first }).first
 
-
-      inventory.nil? ? sum : sum + inventory.price_cents * item
+      inventory.nil? ? sum : sum + inventory.price_cents * item.last
     end
     Money.new(total)
   end
@@ -26,7 +24,7 @@ class Supermarket < ApplicationRecord
     inventories.where(inventories: { product_id: ids }).count
   end
 
-
-
+  def total_matches_name(name)
+    inventories.where(inventories: { product_id: name })
+  end
 end
-
